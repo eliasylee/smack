193d1f5177f1fbd740a002fa1da32efcdce4c46d
eliasylee <eliasylee@gmail.com>
2016-08-30 09:53:33 -0700

    back-end auth completed

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login_user!(@user)
+      render 'api/users/show'
+    else
+      render(json: ["Username or password are incorrect"], status: 401)
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout_user!
+      render json: 'api/users/show'
+    else
+      render(json: ["Nobody signed in"], status: 404)
+    end
+  end
+end
