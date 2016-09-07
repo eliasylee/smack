50f20f66ceec4158b8bbbfaf02247fa2f93da44f
eliasylee <eliasylee@gmail.com>
2016-09-07 09:12:21 -0700

    fixed migration

--- db/migrate/20160907151234_create_controllers.rb
+++
@@ -1,9 +0,0 @@
-class CreateControllers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :controllers do |t|
-      t.string :api/DirectMessages
-
-      t.timestamps
-    end
-  end
-end
