ee36e4499d6e0ca0d89181c6808bcbe5065e0a41
eliasylee <eliasylee@gmail.com>
2016-10-05 19:38:13 -0700

    added color column to users and edited seeds to be create without bang

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907145943) do
+ActiveRecord::Schema.define(version: 20161006023621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160907145943) do
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "color"
   end
 
 end
